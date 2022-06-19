9bc02833e01ff736d348a1a8ae9d7984426adcaa
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2022-06-18 22:05:22 -0500

    reduce file size

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from json_utils import search_books
+from urllib.request import urlopen
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
+
+with urlopen("") as response:
+    
 
 
 @app.route("/books/")
